e9b483b84b5734e81821b51c9c16feb9d99e374b
Philip M Wemegah <your@email.com>
2025-09-25 07:07:54 +0000

    loops

---
+++ loops.py
@@ -0,0 +1,6 @@
+# for seven in range(1, 11):
+#     print(f"7 x {seven} = {7 * seven}")
+
+for  pyramid in range(1, 7):
+    print(" "  "*" * (pyramid) )
+        #   * (5 - pyramid) + "*" * (2 * pyramid - 1))
