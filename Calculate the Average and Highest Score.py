c4516c49f4d91ee49f65d8e3b254961fd17ef133
Philip M Wemegah <philipwemgah990@gmail.com>
2025-10-17 14:51:30 +0000

    Update Calculate the Average and Highest Score.py

--- Calculate the Average and Highest Score.py
+++ Calculate the Average and Highest Score.py
@@ -1,6 +1,14 @@
 def cal_average_score():
-  score_list = []
-  for i in range(5):
-    score = int(input(f"Enter score {i + 1}"))
-    score_list.append(score)
+    score_list = []
+    for i in range(5):
+        score = int(input(f"Enter score {i + 1}"))
+        score_list.append(score)
+    
     print(f"Scores : {score_list}") 
+
+    ave_score = sum(score_list)/len(score_list)
+    print(f"Average Score : {ave_score}")
+    
+    high_score = max(score_list)
+    print(f"Highest Score : {high_score}")
+cal_average_score()
